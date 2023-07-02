9748ecdc0c3a0e63d9751c7cf62131f67b7db47a
srini7150 <srinivasdevops7150@gmail.com>
2023-07-02 16:52:32 +0530

    FIX: convert vars to integers

--- vars/pipelineJava.groovy
+++ vars/pipelineJava.groovy
@@ -14,7 +14,7 @@ def call () {
                         def version_vars = readProperties  file:'version_vars.properties'
                         def VERSION = version_vars['VERSION']
                         def INT_RELEASE_BUILD_NO = version_vars['initial_release_build_number']
-                        def UNIQUE_VERSION_ID = BUILD_NUMBER - INT_RELEASE_BUILD_NO
+                        def UNIQUE_VERSION_ID = BUILD_NUMBER.toInteger() - INT_RELEASE_BUILD_NO.toInteger()
                         echo "version is: ${VERSION}-${UNIQUE_VERSION_ID}"
                         echo "DOCKER_USERNAME is: ${DOCKER_CREDS_USR}"
                         echo "DOCKER_USERNAME is: ${DOCKER_CREDS_PSW}"
