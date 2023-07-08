aec15b793f0ccf002387f6fde9fe4480d862badb
srini7150 <srinivasdevops7150@gmail.com>
2023-07-08 20:26:25 +0530

    update javaPipeline with version strategy

--- vars/pipelineJava.groovy
+++ vars/pipelineJava.groovy
@@ -18,13 +18,16 @@ def call () {
             stage('build') {
                 steps{
                     script {
-                        def version_vars = readProperties  file:'version_vars.properties'
-                        def VERSION = version_vars['VERSION']
-                        def INT_RELEASE_BUILD_NO = version_vars['initial_release_build_number']
-                        def UNIQUE_VERSION_ID = BUILD_NUMBER.toInteger() - INT_RELEASE_BUILD_NO.toInteger()
-                        echo "version is: ${VERSION}-${UNIQUE_VERSION_ID}"
-                        echo "DOCKER_USERNAME is: ${DOCKER_CREDS_USR}"
-                        echo "DOCKER_USERNAME is: ${DOCKER_CREDS_PSW}"
+                        // def version_vars = readProperties  file:'version_vars.properties'
+                        // def VERSION = version_vars['VERSION']
+                        // def INT_RELEASE_BUILD_NO = version_vars['initial_release_build_number']
+                        // def UNIQUE_VERSION_ID = BUILD_NUMBER.toInteger() - INT_RELEASE_BUILD_NO.toInteger()
+                        // echo "version is: ${VERSION}-${UNIQUE_VERSION_ID}"
+                        // echo "DOCKER_USERNAME is: ${DOCKER_CREDS_USR}"
+                        // echo "DOCKER_USERNAME is: ${DOCKER_CREDS_PSW}"
+                        def VERSION = godGetVersion(pipelineParams.module, BRANCH_NAME)
+                        echo "branch is: ${BRANCH_NAME}"
+                        echo "version is:${VERSION}"
                     }
                 }
             }
