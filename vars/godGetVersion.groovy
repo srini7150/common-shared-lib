35a94fa9f5384c68b3506cac522359cd54e92e0f
srini7150 <srinivasdevops7150@gmail.com>
2023-07-08 20:07:12 +0530

    add getversion feature

---
+++ vars/godGetVersion.groovy
@@ -0,0 +1,15 @@
+import com.bnp.god.devops.library.godUtils;
+
+def call (String module, String branch) {
+    def releaseBranch = getReleaseBranch();
+    GodUtils godUtils = new GodUtils();
+    if (branch == "master") {
+        return godUtils.getVersion(getProdVersion(module), branch, releaseBranch);
+    } else if (branch.endsWith("-hotfix")) {
+        def hotfixCount = getHotfixCount(module);
+        return godUtils.getVersion(getProdVersion(module), branch, releaseBranch); + "-hotfix" + hotfixCount;
+    }
+    else {
+        return godUtils.getCounterFileContent(module, branch, releaseBranch);
+    }
+}
