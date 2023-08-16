fd03ace7c1a252c6299e75bacd665de794f5fa30
srini7150 <srinivasdevops7150@gmail.com>
2023-08-17 00:42:35 +0530

    fix getCounterFileContent function call

--- vars/incrementVersionCounter.groovy
+++ vars/incrementVersionCounter.groovy
@@ -3,7 +3,7 @@ import com.bnp.god.devops.library.GodUtils
 def call (String module) {
     lock("verinc") {
         GodUtils godUtils = new GodUtils();
-        def nextVersion = godUtils.nextVersion(getCounterfileContent(module));
+        def nextVersion = godUtils.nextVersion(getCounterFileContent(module));
         def counterscripts = libraryResource("scripts/increment-counter.sh")
         writeFile file: 'increment-counter.sh', text: counterscripts
         sh("chmod 755 increment-counter.sh")
