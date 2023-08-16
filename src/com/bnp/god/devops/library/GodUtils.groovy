f10aef984962a52247bf4acdabaae69ee9b0436a
srini7150 <srinivasdevops7150@gmail.com>
2023-08-16 22:01:11 +0530

    add ist branch into versioning

--- src/com/bnp/god/devops/library/GodUtils.groovy
+++ src/com/bnp/god/devops/library/GodUtils.groovy
@@ -5,13 +5,16 @@ class GodUtils implements Serializable {
     static final String ONE = ".";
     static final String COMMA = ",";
 
-    def getVersion (String version, String branch, String releaseBranch) {
+    def getVersion (String version, String branch, String releaseBranch, String istBranch) {
         version = version.trim();
         if (branch == releaseBranch || branch == 'main' || branch =~ /(hotfix.*)/) {
             return version;
         }
-        else if (branch.startsWith("feature")) {
-            return version + "-SNAPSHOT"
+        else if (branch == istBranch) {
+            return version + "-SNAPSHOT";
+        }
+        else if (branch =~ /(feature.*)/) {
+            return "1.0.0-SNAPSHOT";
         }
     }
 }
--- vars/godVersion.groovy
+++ vars/godVersion.groovy
@@ -2,14 +2,15 @@ import com.bnp.god.devops.library.GodUtils;
 
 def call (String module, String branch) {
     def releaseBranch = getReleaseBranch();
+    def istBranch = getIstBranch();
     GodUtils godUtils = new GodUtils();
     if (branch == "main") {
-        return godUtils.getVersion(getProdVersion(module), branch, releaseBranch);
+        return godUtils.getVersion(getProdVersion(module), branch, releaseBranch, istBranch);
     } else if (branch =~ /(hotfix.*)/) {
         def hotfixCount = getHotfixCounter(module);
-        return godUtils.getVersion(getProdVersion(module), branch, releaseBranch) + "." + hotfixCount;
+        return godUtils.getVersion(getProdVersion(module), branch, releaseBranch, istBranch) + "." + hotfixCount;
     }
     else {
-        return godUtils.getVersion(getCounterFileContent(module), branch, releaseBranch);
+        return godUtils.getVersion(getCounterFileContent(module), branch, releaseBranch, istBranch);
     }
 }
