396beda4f8d4a6364b3195c8b533e7965f721f4a
srini7150 <srinivasdevops7150@gmail.com>
2023-08-18 23:49:28 +0530

    change push command to use url with creds

--- resources/scripts/increment-counter.sh
+++ resources/scripts/increment-counter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Increment version.counter and pushes the code back to git
 
+set -x
 if [ -d temp-verinc ]; then
     rm -rf temp-verinc
 fi
@@ -9,7 +10,7 @@ git config --global user.email 'srinivasdevops7150@gmail.com'
 git config --global user.name 'srini7150'
 git clone https://github.com/srini7150/common-shared-lib.git
 
-cd common-shared-lib && git checkout release && echo $2 > resources/$1/version.counter && git add resources/$1/version.counter && git commit -m "[ci skip] Bumping version to $2 of $1 module" && git push
+cd common-shared-lib && git checkout release && echo $2 > resources/$1/version.counter && git add resources/$1/version.counter && git commit -m "[ci skip] Bumping version to $2 of $1 module" && git push https://${GITHUB_CREDS_USR}:${GITHUB_CREDS_USR}@github.com/srini7150/common-shared-lib.git
 cd ../../
 pwd
 rm -rf temp-verinc
