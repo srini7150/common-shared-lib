f04cc55c6d2d46eb3958ab7e61cdd30dd7f27eaa
srini7150 <srinivasdevops7150@gmail.com>
2023-08-18 22:36:05 +0530

    fix script execution of incVer function

--- vars/incrementVersionCounter.groovy
+++ vars/incrementVersionCounter.groovy
@@ -3,17 +3,11 @@ import com.bnp.god.devops.library.GodUtils
 def call (String module) {
     lock("verinc") {
         GodUtils godUtils = new GodUtils();
-        def counterFileValue = getCounterFileContent(module);
-        echo "counterFileValue is${counterFileValue}";
         def nextVersion = godUtils.nextVersion(getCounterFileContent(module));
-        echo "nextVersion is ${nextVersion}"
-        sh ("ls -la")
         def counterscripts = libraryResource("scripts/increment-counter.sh")
         writeFile file: 'increment-counter.sh', text: counterscripts
-        sh ("ls -la")
         sh("chmod 755 increment-counter.sh")
         echo "Call increment counter with ${module} and ${nextVersion}"
-        sh ("ls -la")
-        sh("./increment-counter.sh ${module} ${nextVersion}")
+        sh("./increment-counter.sh && incVer ${module} ${nextVersion}")
     }
 }
