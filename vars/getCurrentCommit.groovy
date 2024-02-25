a046313a2761dcc3aafdfae9effae8f8622e08e8
srini7150 <srinivasdevops7150@gmail.com>
2024-02-25 23:38:33 +0530

    fix groovy script

--- vars/getCurrentCommit.groovy
+++ vars/getCurrentCommit.groovy
@@ -1,4 +1,4 @@
-def call (String module, String branchName) {
+def call(String module, String branchName) {
     def RepoName = [
         "webapp": "webapp",
         "ansible": "ansible"
@@ -7,7 +7,7 @@ def call (String module, String branchName) {
     def commitidScript = libraryResource("scripts/currentCommit.sh")
     writeFile file: "currentCommitId.sh", text: commitidScript
     def REPO_NAME = RepoName["${module}"]
-    sh("ls && chmod 755 currentCommitId.sh")
-    def currentCommit = sh(returnStdout: true, script: "./currentCommitId.sh && currentCommitId ${REPO_NAME} ${gitlabhost} ${branchName}").trim()
-    return "${currentCommit}"
+    sh "ls && chmod 755 currentCommitId.sh"
+    def currentCommit = sh(returnStdout: true, script: "./currentCommitId.sh ${REPO_NAME} ${gitlabhost} ${branchName}").trim()
+    return currentCommit
 }
